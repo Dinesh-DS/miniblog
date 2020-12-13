6c5ab80db79d1517c6e2afee14a07cee69a4db76
Dinesh-DS <61001493+Dinesh-DS@users.noreply.github.com>
2020-12-13 22:29:51 +0530

    Add files via upload

---
+++ miniblog/urls.py
@@ -0,0 +1,33 @@
+"""miniblog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from blog import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home),
+    path('detail/<id>/', views.post_detail, name='detail'),
+    path('add/', views.add_post, name='add'),
+    path('profile/', views.user_profile, name='profile'),
+    path('edit/<id>/', views.update_post, name='edit'),
+    path('delete/<id>/', views.delete_post, name='delete'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
